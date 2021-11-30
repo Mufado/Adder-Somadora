d969cd7f4f8071e12c89c6f6fab5c5f177b809ac
Gustavo Girão <gustavogiraobg@gmail.com>
2021-11-29 23:46:59 -0300

    First version complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,49 @@
 import React, { Component } from "react";
 import "./App.css"
 
-
-class Addends extends Component {
-    render() {
-        return (
-            <form>
-                <input type="number" />
-            </form>
-        )
-    };
-}
-
-class Total extends Component {
-    render() {
-        return (
-            <h1> Total </h1>
-        )
-    };
-}
-
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = [];
+        this.state = {
+            firstAddend: '',
+            secondAddend: '',
+            sumTotal: 'Resultado'
+        };
+
+        this.handleSumAddends = this.handleSumAddends.bind(this);
+        this.handleGetFirstAddend = this.handleGetFirstAddend.bind(this);
+        this.handleGetSecondAddend = this.handleGetSecondAddend.bind(this);
+    }
+
+    handleGetFirstAddend(e) {
+        this.setState({ firstAddend: Number(e.target.value) });
+    }
+
+    handleGetSecondAddend(e) {
+        this.setState({ secondAddend: Number(e.target.value) });
+    }
+
+    handleSumAddends(e) {
+        this.setState({ sumTotal: this.state.firstAddend + this.state.secondAddend });
+        e.preventDefault();
     }
 
     render() {
+        const title = process.env.REACT_APP_TITLE;
         return (
-            <div>
-                <Addends>  </Addends>
-                <Addends>  </Addends>
-                <Total> </Total>
+            <div className="container">
+                <h1> {title} </h1>
+
+                <div className="calc">
+                    <input type="number" onChange={this.handleGetFirstAddend} />
+                    <h2> + </h2>
+                    <input type="number" onChange={this.handleGetSecondAddend} />
+                    <form onSubmit={this.handleSumAddends}>
+                        <button type="submit"> = </button>
+                    </form>
+                    <h3> {this.state.sumTotal} </h3>
+                </div>
+
             </div>
         )
     };
